dd75b092c89389425c2bf166f3ee8232b856c835
jamesbutler01 <jamesbutler01@hotmail.co.uk>
2019-11-20 13:06:54 +0000

    New maps added for DM

--- Behavioural Testing Unit (Android)/Mymou/app/src/main/java/mymou/task/individual_tasks/TaskDiscreteMazeMapParams.java
+++ Behavioural Testing Unit (Android)/Mymou/app/src/main/java/mymou/task/individual_tasks/TaskDiscreteMazeMapParams.java
@@ -1,12 +1,12 @@
 /**
  * Mazes to be used with TaskDiscreteMaze
- *
+ * <p>
  * Allows you to load the parameters of whichever map you'd like to use on the task
- *
  */
 package mymou.task.individual_tasks;
 
 import android.util.Log;
+
 import mymou.R;
 import mymou.preferences.PreferencesManager;
 
@@ -26,7 +26,7 @@ public class TaskDiscreteMazeMapParams {
         Log.d(TAG, "Loading map ");
 
         map_chosen = preferencesManager.dm_map_selected;
-        Log.d(TAG, "Loading map "+map_chosen);
+        Log.d(TAG, "Loading map " + map_chosen);
 
         LoadImageList();
         Load_x_dimension();
@@ -39,6 +39,19 @@ public class TaskDiscreteMazeMapParams {
     public void LoadImageList() {
 
         int[] imageListMapOne = {
+                R.drawable.aaaaa,
+                R.drawable.aaaab,
+                R.drawable.aaaac,
+                R.drawable.aaaad,
+                R.drawable.aaaae,
+                R.drawable.aaaaf,
+                R.drawable.aaaag,
+                R.drawable.aaaah,
+                R.drawable.aaaai,
+                R.drawable.aaaaj,
+        };
+
+        int[] imageListMapTwo = {
                 R.drawable.aabaa,
                 R.drawable.aabab,
                 R.drawable.aabac,
@@ -54,48 +67,91 @@ public class TaskDiscreteMazeMapParams {
                 R.drawable.aabam,
                 R.drawable.aaban,
                 R.drawable.aabao,
-                R.drawable.aabap,};
-
-        int[] imageListMapTwo = {
-                R.drawable.aaaaa,
-                R.drawable.aaaab,
-                R.drawable.aaaac,
-                R.drawable.aaaad,
-                R.drawable.aaaae,
-                R.drawable.aaaaf,
-                R.drawable.aaaag,
-                R.drawable.aaaah,
-                R.drawable.aaaai,
-                R.drawable.aaaaj,
+                R.drawable.aabap,
         };
 
-        int[][] maps = {imageListMapOne, imageListMapTwo};
+        int[] imageListMapThree = {
+                R.drawable.aadaa,
+                R.drawable.aadab,
+                R.drawable.aadac,
+                R.drawable.aadad,
+                R.drawable.aadae,
+                R.drawable.aadaf,
+                R.drawable.aadag,
+                R.drawable.aadah,
+                R.drawable.aadai,
+                R.drawable.aadaj,
+                R.drawable.aadak,
+                R.drawable.aadal,
+                R.drawable.aadam,
+                R.drawable.aadan,
+                R.drawable.aadao,
+                R.drawable.aadap,
+                R.drawable.aadaq,
+                R.drawable.aadar,
+                R.drawable.aadas,
+                R.drawable.aadat,
+                R.drawable.aadau,
+                R.drawable.aadav,
+                R.drawable.aadaw,
+                R.drawable.aadax,
+                R.drawable.aaday,};
+
+        int[] imageListMapFour = {
+                R.drawable.aaeaa,
+                R.drawable.aaeab,
+                R.drawable.aaeac,
+                R.drawable.aaead,
+                R.drawable.aaeae,
+                R.drawable.aaeaf,
+                R.drawable.aaeag,
+                R.drawable.aaeah,
+                R.drawable.aaeai,
+                R.drawable.aaeaj,
+                R.drawable.aaeak,
+                R.drawable.aaeal,
+                R.drawable.aaeam,
+                R.drawable.aaean,
+                R.drawable.aaeao,
+                R.drawable.aaeap,
+                R.drawable.aaeaq,
+                R.drawable.aaear,
+                R.drawable.aaeas,
+                R.drawable.aaeat,
+                R.drawable.aaeau,
+                R.drawable.aaeav,
+                R.drawable.aaeaw,
+                R.drawable.aaeax,
+                R.drawable.aaeay,};
+
+
+        int[][] maps = {imageListMapOne, imageListMapTwo, imageListMapThree, imageListMapFour};
 
         imageList = maps[map_chosen];
     }
 
     public void Load_x_dimension() {
-        int[] x_dimensions = {4, 1};
+        int[] x_dimensions = {1, 4, 5, 5};
         x_size = x_dimensions[map_chosen];
     }
 
     public void Load_y_dimension() {
-        int[] y_dimensions = {4, 10};
+        int[] y_dimensions = {10, 4, 5, 5};
         y_size = y_dimensions[map_chosen];
     }
 
     public void Load_torus() {
-        boolean[] toruses = {true, false};
+        boolean[] toruses = {false, true, true, true};
         torus = toruses[map_chosen];
     }
 
     public void Load_numNeighbours() {
-        int[] numNeighbourss = {4, 2};
+        int[] numNeighbourss = {2, 4, 4, 4};
         numNeighbours = numNeighbourss[map_chosen];
     }
 
     public void Load_maxDistance() {
-        int[] maxDistances = {4, 10};
+        int[] maxDistances = {10, 4, 4, 4};
         maxDistance = maxDistances[map_chosen];
     }
 
